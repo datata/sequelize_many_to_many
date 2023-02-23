3e7196f4f35b1aa7989651f081f11f3ae5ccbdaf
datata <dani@danitarazona.dev>
2023-02-23 23:16:23 +0100

    feat: model product comment relation

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const express = require('express');
+const {Product} = require('./models/index')
+const {Comment} = require('./models/index')
+
+
 const app = express();
 
 app.use(express.json());
@@ -7,6 +11,18 @@ const PORT = 3000;
 
 app.get('/welcome', (req, res) => {
     return res.send('Welcome to my app');
+})
+
+app.get('/products', async(req, res) => {
+    const products = await Product.findAll({ include: Comment });
+
+    return res.json(products);
+})
+
+app.get('/comments', async(req, res) => {
+    const comments = await Comment.findAll({ include: Product});
+
+    return res.json(comments);
 })
 
 app.listen(3000, () => console.log('Server running'));
